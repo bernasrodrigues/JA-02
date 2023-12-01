a7925310cd91eda574a253fffc792e202b115cb0
bernasrodrigues <47544028+bernasrodrigues@users.noreply.github.com>
2023-12-01 17:14:34 +0000

    UI bullet count
    added ui bullet indication

---
+++ Game/JA02/Assets/Scripts/UI/AmmoCount.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class AmmoCount : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public bool ammoReserveInfinite;
+
+    void OnEnable()
+    {
+        Weapon.OnBulletChange += UpdateUI;
+    }
+
+
+    public void UpdateUI(int bulletsLeft, int ammoReserve)
+    {
+        if (ammoReserveInfinite)
+        {
+            text.text = bulletsLeft + " / ∞";
+        }
+        else
+        {
+            text.text = bulletsLeft + " / " + ammoReserve;
+        }
+    }
+}
